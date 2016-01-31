fff4bab62e15d1bc51e5f3a23a534983119620fc
Alex Baratti <alex.techtalk4geeks@gmail.com>
2016-01-30 20:05:04 -0800

    Initial commit - Can return basic Quizlet info

---
+++ app/src/main/java/com/techtalk4geeks/studie/MainActivity.java
@@ -0,0 +1,249 @@
+package com.techtalk4geeks.studie;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.util.Log;
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends Activity {
+
+    public static final String S = "Studie";
+
+    Button linkQuizletAccount;
+    Button findSetButton;
+    EditText quizletLinkEditText;
+    AlertDialog invalidURLDialog;
+    AlertDialog noURLDialog;
+    AlertDialog itWorkedDialog;
+    String quizletTitle = "null";
+
+    String quizletLink;
+    String apiLink;
+    int substringStart;
+    int substringEnd;
+    int charCounter = 0;
+
+    String setJSON;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getActionBar().setDisplayUseLogoEnabled(true);
+        getActionBar().setDisplayShowHomeEnabled(true);
+        getActionBar().setLogo(R.drawable.studieactionbar);
+
+        invalidURLDialog = new AlertDialog.Builder(MainActivity.this).create();
+        invalidURLDialog.setTitle("Invalid URL");
+        invalidURLDialog.setMessage("The link you provided was not a full Quizlet Link. Note that short URLs and URLs that redirect are not supported at this time.");
+        invalidURLDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        noURLDialog = new AlertDialog.Builder(MainActivity.this).create();
+        noURLDialog.setTitle("No URL Provided");
+        noURLDialog.setMessage("Please paste a Quizlet link in the field to find a set.");
+        noURLDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        itWorkedDialog = new AlertDialog.Builder(MainActivity.this).create();
+        itWorkedDialog.setTitle("It worked!");
+        itWorkedDialog.setMessage("Yay!\n" + quizletTitle);
+        itWorkedDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        linkQuizletAccount = (Button) this.findViewById(R.id.signInButton);
+        findSetButton = (Button) this.findViewById(R.id.findSetButton);
+        quizletLinkEditText = (EditText) this.findViewById(R.id.quizletLinkEditText);
+
+        quizletLinkEditText.setMaxLines(1);
+        quizletLinkEditText.setImeActionLabel("Find", 66);
+        quizletLinkEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((actionId == EditorInfo.IME_ACTION_SEARCH) || ((event.getKeyCode() == KeyEvent.KEYCODE_ENTER) && (event.getAction() == KeyEvent.ACTION_DOWN))) {
+                    //TODO: Start Quizlet Set intent
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        findSetButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(S, "onClick received for findSetButton");
+                quizletLink = String.valueOf(quizletLinkEditText.getText().toString());
+                if (quizletLink.contains("quizlet.com/")) {
+                    Log.d(S, "quizletLink contains required domain");
+                    for (int i = 10; i < quizletLink.length(); i++) {
+                        if (quizletLink.charAt(i) == '/') {
+                            charCounter += 1;
+                            if (charCounter == 1) {
+                                substringStart = i + 1;
+                            } else if (charCounter == 2) {
+                                substringEnd = i;
+                                break;
+                            }
+                        }
+                    }
+                    apiLink = "https://api.quizlet.com/2.0/sets/"
+                            + quizletLink.substring(substringStart, substringEnd)
+                            + "?client_id=brgUUPyxDF&whitespace=1";
+                    Log.i(S, "quizletLink = " + quizletLink);
+                    Log.i(S, "apiLink = " + apiLink);
+                    try {
+                        new QuizletAPIOperations().execute(apiLink);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    Log.e(S, "Invalid URL");
+                    invalidURLDialog.show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public String getSet(String APILink) throws Exception {
+        URL url = new URL(APILink);
+        URLConnection connection;
+        connection = url.openConnection();
+
+        HttpURLConnection httpConnection = (HttpURLConnection) connection;
+
+        int responseCode = httpConnection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            InputStream in = httpConnection.getInputStream();
+            String jsonStr = getStringFromInputStream(in);
+            return jsonStr;
+        } else {
+            return null;
+        }
+    }
+
+    private static String getStringFromInputStream(InputStream is) {
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private class QuizletAPIOperations extends
+            AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.i(S, "Made it to onPreExecute()");
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            Log.i(S, "Made it to doInBackground()");
+            try {
+                setJSON = getSet(params[0]);
+                final QuizletSet quizletSet = new QuizletSet(setJSON);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        itWorkedDialog.setMessage(quizletSet.getDebugSummary());
+                        itWorkedDialog.show();
+                        //TODO Show Set
+                    }
+                });
+
+            } catch (Exception e) {
+                Log.e(S, "Error After Program Start: " + e);
+            }
+
+            return "Error";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(S, "Reached onPostExecute, Result = " + setJSON);
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+        }
+    }
+}
---
+++ app/src/main/java/com/techtalk4geeks/studie/QuizletSet.java
@@ -0,0 +1,54 @@
+package com.techtalk4geeks.studie;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alex on 1/30/16.
+ */
+public class QuizletSet {
+
+    String title;
+    String description;
+    String createdBy;
+    String URL;
+    int termCount;
+    ArrayList<String> terms;
+    String termLanguage;
+    ArrayList<String> definitions;
+    String definitionLanguage;
+
+    public QuizletSet(String jsonString) throws Exception {
+        JSONObject jsonOb;
+        jsonOb = new JSONObject(jsonString);
+
+        title = jsonOb.getString("title");
+        description = jsonOb.getString("description");
+        createdBy = jsonOb.getString("created_by");
+        URL = jsonOb.getString("url");
+        try {
+            termCount = Integer.parseInt(jsonOb.getString("term_count"));
+        } catch (NumberFormatException nf) {
+            Log.e("Studie", "NUMBER FORMAT EXCEPTION ON TERM COUNT!!!");
+            termCount = -1;
+        }
+        termLanguage = jsonOb.getString("lang_terms");
+        definitionLanguage = jsonOb.getString("lang_definitions");
+    }
+
+    public String getDebugSummary() {
+        Log.i("Studie", "--- " + title + "---\n" + createdBy + "\n" + description);
+        return "--- " + title + "---\n" + createdBy + "\n" + description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
